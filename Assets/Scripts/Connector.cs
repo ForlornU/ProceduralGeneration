5ffbb4d740a9dd6bd04cb72b4b430560c92b30a5
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-02-09 03:05:36 +0100

    Tiles and connectors

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Connector : MonoBehaviour
+{
+    uint id;
+
+    public void setID(uint _id)
+    {
+        id = _id;
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    [SerializeField]
+    private uint id;
 
-
+    void FindMyConnectors()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<Connector>())
+            {
+                child.GetComponent<Connector>().setID(id);
+            }
+        }
+    }
 }
