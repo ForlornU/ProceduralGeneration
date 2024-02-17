471276df39a025ed6c51fe18677139d5a346a853
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-02-17 03:49:59 +0100

    removed test

--- Assets/Scripts/TileGeneration/Tile.cs
+++ Assets/Scripts/TileGeneration/Tile.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
@@ -9,7 +8,6 @@ public class Tile : MonoBehaviour
     [SerializeField] uint tileID = 0;
 
     [Header("wfc test")]
-    [SerializeField] bool randomdlyRotate90DegreesTest = false;
     ///options
     bool collapsed = false;
     int entropy = 0;
@@ -23,12 +21,7 @@ public class Tile : MonoBehaviour
             if (overrideID)
                 connector.connectorID = tileID;
             connector.parentTile = this;
-        }
-        
-
-        if (randomdlyRotate90DegreesTest && Random.value > 0.5f)
-            transform.Rotate(transform.up, 90f);
-            
+        }           
     }
 
     int GetEntropy()
