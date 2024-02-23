0ba8ed64e6034dd0d450c637a85f678c033d826c
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-02-23 17:33:52 +0100

    rndwalk cleanup

--- Assets/Scripts/TileGeneration/GenerationModules/Module_RandomWalk.cs
+++ Assets/Scripts/TileGeneration/GenerationModules/Module_RandomWalk.cs
@@ -1,64 +1,18 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Module_RandomWalk : GenerationModule
 {
-    //Stack<Connector> connectors = new Stack<Connector>();
-
-    [SerializeField] int randombranches = 0;
+    [SerializeField, Range(0, 99)] int additionalBranches = 0;
 
     public override int Sort(ModuleReferenceData data)
     {
-        //PushStartingConnectors(connectors);
+        int range = data.connectors.Count - (data.lastTile.validTiles + additionalBranches);
 
+        int rndIndex = Random.Range(range, data.connectors.Count);
 
-        //if (canProcessConnector(this.connectors.Pop()))
-        //    Debug.Log("Valid connector");
+        if (data.lastTile.validTiles <= 0)
+            rndIndex = data.connectors.Count - 1;
 
-        //return Random.Range(data.connectors.Count -6, data.connectors.Count - 1);//connectorsToSpawn[connectorIndex].parentTile.connectors.Count - 1); // randomize between the closest options
-        int min = data.connectors.Count - data.lastTile.connectors.Count-1;
-        int r = Random.Range(min, data.connectors.Count);
-        return r;
-
-        Vector3 pos;
-         
-        if (data.lastTile == null)
-        {
-            pos = data.walkerPosition;
-        }
-        else
-        {
-            pos = data.lastTile.transform.position;
-        }
-
-        data.connectors.Sort((x, y) => Vector3.Distance(x.transform.position, pos).CompareTo(Vector3.Distance(y.transform.position, pos)));
-
-        int roof = data.connectors[data.connectorsIndex].parentTile.connectors.Count + randombranches;
-        roof = Mathf.Clamp(roof, 0, data.connectors.Count - 1);
-
-
-
-        return Random.Range(0, roof);
-        //return 0;
+        return rndIndex;
     }
-
-    //private void PushStartingConnectors(List<Connector> startingConnectors)
-    //{
-    //    foreach (var connector in startingConnectors)
-    //    {
-    //        connectors.Push(connector);
-    //    }
-
-        //}
-
-        //bool canProcessConnector(Connector c)
-        //{
-        //    currentConnector = c;
-
-        //    This happens thousands of times, so we don't want to log it. But also find a way to avoid it
-        //    if (c == null || c.isOccupied)
-        //        return false;
-
-        //    return true;
-        //}
 }
