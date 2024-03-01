56df9aabc3905c20e24ef0639a0087fb97751013
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-03-01 16:40:52 +0100

    If-case

--- Assets/Scripts/TileGeneration/GenerationModules/Module_RandomWalk.cs
+++ Assets/Scripts/TileGeneration/GenerationModules/Module_RandomWalk.cs
@@ -8,13 +8,21 @@ public class Module_RandomWalk : GenerationModule
 
     public override int Sort(ModuleReferenceData data)
     {
-        //Range is a measure from the last placed tile to 0 which is the first placed tile
-        // if random branch, let the range be 0 which is 'any placed tile'
+        //Range is a measure from the last placed tile to 0 which is the first placed tile. if random branch, 0 is 'any placed tile'
         int lastPlacedTilesRange = data.connectors.Count - (data.lastTile.validTiles + splitting);
-        int range = Random.value < branchChance ? 0 : lastPlacedTilesRange;
+        int range;
+
+        if (Random.value < branchChance)
+            range = 0;
+        else
+            range = lastPlacedTilesRange;
 
         // If no valid tiles, again return the full lenght of all tiles to randomize
-        int rndIndex = data.lastTile.validTiles <= 0 ? data.connectors.Count - 1 : Random.Range(range, data.connectors.Count);
+        int rndIndex;
+        if (data.lastTile.validTiles <= 0)
+            rndIndex = data.connectors.Count - 1;
+        else
+            rndIndex = Random.Range(range, data.connectors.Count);
 
         return rndIndex;
     }
