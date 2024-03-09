753dcd0c2507601371d788db6b008ad14d7e1285
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-03-09 02:15:38 +0100

    Voxel Initial

---
+++ Assets/Scripts/TileGeneration/Voxel/Voxel.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public struct Voxel
+{
+    public Vector3 position;
+    public bool isActive;
+
+    public VoxelType type; // Using the VoxelType enum
+    public enum VoxelType
+    {
+        Air,    // Represents empty space
+        Grass,  // Represents grass block
+        Stone,  // Represents stone block
+                // Add more types as needed
+    }
+
+    public Voxel(Vector3 position, VoxelType type, bool isActive = true)
+    {
+        this.position = position;
+        this.type = type;
+        this.isActive = isActive;
+    }
+}
