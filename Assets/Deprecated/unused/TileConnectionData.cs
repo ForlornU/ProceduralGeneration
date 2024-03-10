42e4ea39bad2eaf0c01a28b0d98e9f62716f0365
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-03-10 20:49:21 +0100

    104 deprecated files removed

--- Assets/Deprecated/unused/TileConnectionData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TileConnectionData", menuName = "ScriptableObjects/TileConnectionData", order = 1)]
-public class TileConnectionData : ScriptableObject
-{
-
-}
